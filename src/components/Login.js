906c26860b9143588abbefcf3565a52751b9b6fb
Jhon Serna Durango <jhon.serna@sofka.com.co>
2022-03-03 22:16:42 -0500

    Addedo background image

--- src/components/Login.js
+++ src/components/Login.js
@@ -23,10 +23,24 @@ const Content = styled.div`
   height: 100%;
 `;
 
+const BgImage = styled.div`
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url("/images/login-background.jpg");
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`;
 function Login() {
   return (
     <Container>
-      <Content>Content</Content>
+      <Content>
+        <BgImage />
+      </Content>
     </Container>
   );
 }
