d154fd9ad918446206c66d740aa479a129a20427
Jhon Serna Durango <jhon.serna@sofka.com.co>
2022-03-03 22:31:34 -0500

    Added description

--- src/components/Login.js
+++ src/components/Login.js
@@ -43,7 +43,6 @@ const CTA = styled.div`
   flex-direction: column;
 `;
 
-
 const CTALogoOne = styled.img`
   margin-bottom: 12px;
   max-width: 600px;
@@ -52,12 +51,44 @@ const CTALogoOne = styled.img`
   width: 100%;
 `;
 
+const SignUp = styled.a`
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 1.5px;
+  font-size: 18px;
+  padding: 16.5px 0;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  &:hover {
+    cursor: pointer;
+    background-color: #0483ee;
+  }
+`;
+
+const Description = styled.p`
+  color: hsla(0, 0%, 95.3%, 1);
+  font-size: 11px;
+  margin: 0 0 24px;
+  line-height: 1.5;
+  letter-spacing: 1.5px;
+`;
+
+
 function Login() {
   return (
     <Container>
       <Content>
         <CTA>
           <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+          <SignUp>GET ALL THERE</SignUp>
+          <Description>
+            Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription. As of 03/26/21, the price of Disney+
+            and The Disney Bundle will increase by $1.
+          </Description>
         </CTA>
         <BgImage />
       </Content>
