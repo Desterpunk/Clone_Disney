7ddaaf547e55cf6b84b3c7387721978862b15230
Jhon Serna Durango <jhon.serna@sofka.com.co>
2022-03-04 19:36:05 -0500

    Add login button

--- src/components/Header.js
+++ src/components/Header.js
@@ -94,6 +94,22 @@ const NavMenu = styled.div`
   } */
 `;
 
+const Login = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+    cursor: pointer;
+  }
+`;
+
 function Header() {
   return (
     <Nav>
@@ -128,6 +144,7 @@ function Header() {
           <span>SERIES</span>
         </a>
       </NavMenu>
+      <Login>Login</Login>
     </Nav>
   );
 }
