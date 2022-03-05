838ccb8e9493d0605be8023b37e8060733321985
Jhon Serna Durango <jhon.serna@sofka.com.co>
2022-03-04 19:46:09 -0500

    firebase file created

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8Pp1eKV-c5e5zveEwkchq178AqKSaYCw",
+  authDomain: "disney-clone-492b4.firebaseapp.com",
+  projectId: "disney-clone-492b4",
+  storageBucket: "disney-clone-492b4.appspot.com",
+  messagingSenderId: "1041644896161",
+  appId: "1:1041644896161:web:e49b892f0ff0f3e89a95e0",
+  measurementId: "G-FZ1DTQED72",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firesore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db;
